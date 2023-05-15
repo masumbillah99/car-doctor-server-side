3b0681db4507a2ddd1920141332b5a5b38c03fb5
Masum Billah <masumbillahyusuf1621@gmail.com>
2023-05-15 12:07:43 +0600

    add jwt token and verify jwt token

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const jwt = require("jsonwebtoken");
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 const app = express();
@@ -20,20 +21,58 @@ const client = new MongoClient(uri, {
   },
 });
 
+// jwt token verify and handle 401, 403
+const verifyJWT = (req, res, next) => {
+  // console.log("hitting verify JWT");
+
+  const authorization = req.headers?.authorization;
+  // console.log(authorization);
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  const token = authorization.split(" ")[1];
+  // console.log("token inside", token);
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (error, decoded) => {
+    if (error) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    console.log(decoded);
+    next();
+  });
+};
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const serviceCollection = client.db("carDoctor").collection("services");
     const bookingCollection = client.db("carDoctor").collection("bookings");
 
+    // jwt - json_web_tokens
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      // console.log(user);
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+      // console.log(token);
+      res.send({ token });
+    });
+
+    // services routes
     app.get("/services", async (req, res) => {
       const cursor = serviceCollection.find();
       const result = await cursor.toArray();
       res.send(result);
     });
 
+    // service data with specific id
     app.get("/services/:id", async (req, res) => {
       const id = req.params.id;
       const query = { _id: new ObjectId(id) };
@@ -46,18 +85,28 @@ async function run() {
       res.send(result);
     });
 
-    // bookings some data
-    app.get("/bookings", async (req, res) => {
+    // bookings routes
+    app.get("/bookings", verifyJWT, async (req, res) => {
+      const decoded = req.decoded;
+      // console.log(decoded);
+      console.log(req.query.email);
+      if (decoded?.email !== req.query?.email) {
+        return res
+          .status(401)
+          .send({ error: true, message: "unauthorized access" });
+      }
+
+      // console.log(req.headers.authorization);
       // console.log(req.query.email); // returns a empty object
       let query = {};
       if (req.query?.email) {
-        query = { email: req.query.email };
+        query = { email: req.query?.email };
       }
       const result = await bookingCollection.find(query).toArray();
       res.send(result);
     });
 
-    // bookings all data
+    // bookings single data
     app.post("/bookings", async (req, res) => {
       const booking = req.body;
       //   console.log(booking);
@@ -66,8 +115,20 @@ async function run() {
     });
 
     // update booking items
-    app.put("/bookings/:id", async (req, res) => {
+    app.patch("/bookings/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
       const updateBooking = req.body;
+      // console.log(updateBooking);
+
+      // create a document that sets the status of the booking service
+      const updateDoc = {
+        $set: {
+          status: updateBooking.status,
+        },
+      };
+      const result = await bookingCollection.updateOne(filter, updateDoc);
+      res.send(result);
     });
 
     // delete a bookings data
